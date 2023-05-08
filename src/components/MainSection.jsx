5d7c277e08bde55125a190f8ede6c9f33b984640
carloscaste-LV <82343747+carloscaste-LV@users.noreply.github.com>
2023-05-08 03:17:39 -0700

    Dibubujo de lineas funcionando -> Crear en carpeta diferente funcion drawSensorLine

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,7 +5,11 @@ import './css/SectionMain.css'
 
 const degreesToRadians = (deg) => deg * Math.PI / 180;
 
-
+const lines = [
+  {x1:0,y1:0,x2:300,y2:300},
+  {x1:0,y1:0,x2:350,y2:350},
+  {x1:100,y1:300,x2:350,y2:550},
+];
 
 
 const getRobotRotationAngle = (robotElement) => {
@@ -16,8 +20,8 @@ const getRobotRotationAngle = (robotElement) => {
 
 
 const SENSOR_MEASURES = [
-  { angle: 0, distance: 100 },
-  { angle: 120, distance: 120 },
+  { angle: 0, distance: 140 },
+  { angle: 120, distance: 140 },
   { angle: 240, distance: 140 },
 ];
 
@@ -31,7 +35,7 @@ export default function MainSection(props) {
 
   //Measueres distance sensor
   const [sensorDistances, setSensorDistances] = useState(SENSOR_MEASURES.map((sensor) => sensor.distance));
-
+  const [hrHeight, setHrHeight] = useState([{hrY:140},{hrY:140},{hrY:140}])
   // State that will be used to compare past values with current values measured by sensors.
   const [sensorPositions, setSensorPositions] = useState([
     { x: 0, y: 0 }, 
@@ -82,29 +86,70 @@ function drawSensorLine(sensor, p5, measure, deg, robotDeg = 0, drawActive) {
   const x = positionSensorX + measure * Math.cos(mDeg + rDeg) ;
   const y = positionSensorY - measure * Math.sin(mDeg + rDeg) ;
 
-  setSensorDistances
+  
 
 
   // Calcular la distancia entre la posición actual y anterior del sensor
   const prevPosition = sensorPositions[parseInt(sensor.slice(-1)) - 1];
   const distance = Math.sqrt((prevPosition.x - x) ** 2 + (prevPosition.y - y) ** 2);
-  const intersection = doLinesIntersect(positionSensorX, positionSensorY, x, y, 0, 0, 300, 300)
-
-  // Dibujar la línea solo si la distancia es menor al umbral establecido y la línea del sensor se intersecta con la pared
-  if (distance < 30 && intersection ) {
-    p5.stroke(255);
-    p5.line(intersection[0], intersection[1],intersection[0], intersection[1]);
-    // p5.line(pr,positionSensorY,x, y);
+  
+  
+  // Calcular punto de interseccion entre medicion del sensor y pared simulada
+  //const intersection = doLinesIntersect(positionSensorX, positionSensorY, x, y, 0, 0, 300, 300)
+  let intersection = null;
+  for (let i = 0; i < lines.length; i++) {
+    const currentIntersection = doLinesIntersect(
+      positionSensorX,
+      positionSensorY,
+      x,
+      y,
+      lines[i].x1,
+      lines[i].y1,
+      lines[i].x2,
+      lines[i].y2
+    );
+    if (currentIntersection) {
+      intersection = currentIntersection;
+      break;
+    }
   }
+  if (distance < 60 && intersection) {
+    p5.stroke(255);
+    p5.line(intersection[0], intersection[1], intersection[0], intersection[1]);
+  }
+  // Actualizar la posición anterior del sensor
+  setSensorPositions(prevPositions => [
+    ...prevPositions.slice(0, parseInt(sensor.slice(-1)) - 1),
+    {x, y},
+    ...prevPositions.slice(parseInt(sensor.slice(-1)))
+  ]);
 
-  // // Actualizar la posición anterior del sensor
-  // setSensorPositions(prevPositions => [
+
+  if (sensor === "sensor1") {
+    setHrHeight(prevState => [
+      intersection ? {hrY: positionSensorY - intersection[1]} : {hrY: 140}, // primer objeto actualizado
+      prevState[1], // segundo objeto sin cambios
+      prevState[2], // tercer objeto sin cambios
+    ]);
+  } if (sensor === "sensor2") {
+    setHrHeight(prevState => [
+      prevState[0], // primer objeto sin cambios
+      intersection ? {hrY: Math.sqrt((intersection[0]-positionSensorX)**2+(intersection[1]-positionSensorY)**2)} : {hrY: 140}, // segundo objeto actualizado
+      prevState[2], // tercer objeto sin cambios
+    ]);
+  } if (sensor === "sensor3") {
+    setHrHeight(prevState => [
+      prevState[0], // primer objeto sin cambios
+      prevState[1], // segundo objeto sin cambios
+      intersection ? {hrY:  Math.sqrt((intersection[0]-positionSensorX)**2+(intersection[1]-positionSensorY)**2)} : {hrY: 140}, // tercer objeto actualizado
+    ]);
+  }
+  console.log(hrHeight[0]);
+  // setHrHeight(prevPositions => [
   //   ...prevPositions.slice(0, parseInt(sensor.slice(-1)) - 1),
-  //   {x, y},
+  //   {hry:intersection[1]},
   //   ...prevPositions.slice(parseInt(sensor.slice(-1)))
   // ]);
-
-  
 }
 
 
@@ -125,7 +170,7 @@ const draw = (p5) => {
         {[1, 2, 3].map((i) => (
           <div key={i} className={`sensor r${i}`}>
             <hr
-              style={{height : '200px'}}
+              style={{height : `${hrHeight[i-1].hrY -5}px`}}
               id={`distanceSensor${i}`}
               className={`mensure distanceSensor${i}`}
             />
